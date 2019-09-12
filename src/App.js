d890e14b908637dc4f8de1d153f49682e1073483
Никита Родионов <nikita.rodionov.job@yandex.ru>
2019-09-12 11:52:01 +0300

    lesson_16

--- src/App.js
+++ src/App.js
@@ -1,41 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {count, length} from './numbers';
 
-const school = 'WebHeroSchool'
-let age = 1;
-let autumn = true;
+const elem = (<div>Hello World!</div>);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit <code>src/App.js</code> and save to reload.</p>
-        <p>Hello!</p>
-        <p>В этом году сообществу {school} исполнился {age} год. В 2020-ом году сообществу исполнится {age + 1} года</p>
-        {autumn === true &&
-        <p>На дворе осень, а это значит, что:</p>
-        }
-        {(autumn) ? 
-          <p style={{color: '#00FFFF'}}>"Winter is coming!" :))</p> : 
-          <p>Это React приложение не знает какое сейчас время года, но точно не осень!</p>
-        }
-        <p>"true"{true}, "false"{false}, "null"{null} и "undefined"{undefined} не выводятся в разметку</p>
-        <p>Error {count * length}</p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export {elem};
--- src/numbers.js
+++
@@ -1,4 +0,0 @@
-const count = 89;
-const length = 11;
-
-export {count, length};
