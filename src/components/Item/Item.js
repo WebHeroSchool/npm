75b50434ffa6aca96da2728bceeb0fb0af828f40
Никита Родионов <nikita.rodionov.job@yandex.ru>
2019-09-14 19:27:12 +0300

    lesson_24

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,9 +4,9 @@ import styles from '../App/App.module.css';
 
 const Item = ({ value, isDone }) => (<span className={
   classnames({
-	   [styles.item]: true,
-	   [styles.done]: isDone
-	 })
+    [styles.item]: true,
+    [styles.done]: isDone
+  })
 }>
   {value}
 </span>);
