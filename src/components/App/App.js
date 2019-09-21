f8caeb131d7fc9b151d4b7198fddb3f96fe8dad8
lunar616 <lunar616north@gmail.com>
2019-09-21 17:59:09 +0400

    lesson 27

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,33 +4,36 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const App = () => {
-  const items = [
-    {
-      value: 'Изучить React',
-      isDone: false
-    },
-    {
-      value: 'Подготовиться к собеседованию',
-      isDone: false
-    },
-    {
-      value: 'Найти работу!',
-      isDone: false
-    },
-    {
-      value: 'Тест',
-      isDone: true,
-    }
-  ];
-  const counctUnfulfilled = items.filter(item => item.isDone === false);
-
-  return (
-  <div className={styles.wrap}>
-    <h1 className={styles.title}>todos</h1>
-    <InputItem />
-    <ItemList items={items} />
-    <Footer count={counctUnfulfilled.length} />
-  </div>)};
+class App extends React.Component {
+  render() {
+    const items = [
+      {
+        value: 'Изучить React',
+        isDone: false
+      },
+      {
+        value: 'Подготовиться к собеседованию',
+        isDone: false
+      },
+      {
+        value: 'Найти работу!',
+        isDone: false
+      },
+      {
+        value: 'Тест',
+        isDone: true
+      },
+    ];
+    const counctUnfulfilled = items.filter(item => item.isDone == false);
+  
+    return (
+    <div className={styles.wrap}>
+      <h1 className={styles.title}>todos</h1>
+      <InputItem />
+      <ItemList items={items} />
+      <Footer count={counctUnfulfilled.length} />
+    </div>)
+  }
+}
 
 export default App;
