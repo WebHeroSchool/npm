143d30bb3c25af93b2ae6b57cb6f421dd67426c1
lunar616 <lunar616north@gmail.com>
2019-09-22 13:30:59 +0400

    lesson 28

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,32 +5,35 @@ import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
 class App extends React.Component {
+  state = {
+    items: [
+    {
+       value: 'Изучить React',
+       isDone: false
+     },
+    {
+       value: 'Подготовиться к собеседованию',
+       isDone: false
+     },
+     {
+       value: 'Найти работу!',
+       isDone: false
+     },
+     {
+      value: 'Тест',
+      isDone: true
+    },
+  ]
+}
+
   render() {
-    const items = [
-      {
-        value: 'Изучить React',
-        isDone: false
-      },
-      {
-        value: 'Подготовиться к собеседованию',
-        isDone: false
-      },
-      {
-        value: 'Найти работу!',
-        isDone: false
-      },
-      {
-        value: 'Тест',
-        isDone: true
-      },
-    ];
-    const counctUnfulfilled = items.filter(item => item.isDone == false);
+    const counctUnfulfilled = this.state.items.filter(item => item.isDone == false);
   
     return (
     <div className={styles.wrap}>
       <h1 className={styles.title}>todos</h1>
       <InputItem />
-      <ItemList items={items} />
+      <ItemList items={this.state.items} />
       <Footer count={counctUnfulfilled.length} />
     </div>)
   }
