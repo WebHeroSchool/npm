bf04b92ef278bb9374ed9d9d78b516e8f1c8c122
lunar616 <deadlunardeadshe@yandex.ru>
2019-09-23 13:17:13 +0400

    lesson 30

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -6,13 +6,20 @@ import itemListStyles from './ItemList.module.css';
 
 const ItemList = ({ items, onClickDone }) => (
   <ul className={itemListStyles.list}>
-    {items.map(item => <li key={item.value} className={
+    {items.map(item => 
+    <li key={item.value} className={
       classnames({
         [styles['icon-of-done']]: item.isDone,
         [styles['icon-of-unfinished']]: !item.isDone,
       })
     }>
-      <Item value={item.value} isDone={item.isDone} onClickDone={onClickDone} /></li>)}
+      <Item
+        value={item.value} 
+        isDone={item.isDone} 
+        id={item.id} 
+        onClickDone={onClickDone} 
+      />
+    </li>)}
   </ul>);
 
 export default ItemList;
